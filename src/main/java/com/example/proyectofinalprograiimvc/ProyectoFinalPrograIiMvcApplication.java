109fcc251ad836168080381107f2ba22d00aaea6
Emerson Sec <emersp20@gmail.com>
2023-11-23 23:12:12 -0600

    arreglado bug guardar

--- src/main/java/com/example/proyectofinalprograiimvc/ProyectoFinalPrograIiMvcApplication.java
+++ src/main/java/com/example/proyectofinalprograiimvc/ProyectoFinalPrograIiMvcApplication.java
@@ -1,15 +1,21 @@
 package com.example.proyectofinalprograiimvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 
 @EnableCaching
 @SpringBootApplication
-public class ProyectoFinalPrograIiMvcApplication {
+public class ProyectoFinalPrograIiMvcApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFinalPrograIiMvcApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectoFinalPrograIiMvcApplication.class);
+	}
 }
+
